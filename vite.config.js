df8453dda9efb71f99d78a19d6d99a7e5573b822
laboratorys <359916450@qq.com>
2026-03-03 17:40:03 +0800

    🔖 v1.0.6

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 import monkey, { cdn, util } from "vite-plugin-monkey";
 
 // https://vitejs.dev/config/
@@ -20,14 +20,14 @@ export default defineConfig({
           "https://scriptcat.org/lib/513/2.0.1/ElementGetter.js#sha256=V0EUYIfbOrr63nT8+W7BP1xEmWcumTLWu2PXFJHh5dg=",
           util.dataUrl(`window.elmGetter=elmGetter`),
         ],
-        version: "1.0.5",
+        version: "1.0.6",
       },
       build: {
         externalGlobals: {
           vue: cdn
             .npmmirror("Vue", "dist/vue.global.prod.js")
             .concat(
-              "https://registry.npmmirror.com/vue-demi/0.14.10/files/lib/index.iife.js"
+              "https://registry.npmmirror.com/vue-demi/0.14.10/files/lib/index.iife.js",
             )
             .concat(util.dataUrl(";window.Vue=Vue;")),
           jquery: cdn.npmmirror("jQuery"),
