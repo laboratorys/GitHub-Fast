ceab0f12d95bc3b4e9ec2f4501ea2eeb8065bad3
laboratorys <359916450@qq.com>
2026-04-23 17:27:48 +0800

    🐛  fix(#2): Disable preflight styles to avoid global resets

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <n-config-provider :theme="currentTheme">
+  <n-config-provider :theme="currentTheme" preflight-style-disabled>
     <GitHub />
   </n-config-provider>
 </template>
 <script setup>
-import GitHub from "./components/GitHub.vue";
 import { NConfigProvider } from "naive-ui";
+import GitHub from "./components/GitHub.vue";
 import { currentTheme } from "./utils/themeMode.js";
 </script>
 <style scoped></style>
